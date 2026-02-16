32f251fa00f508b57bb1019d14681e12d4afb8b6
as6314-srm <as6314@srmist.edu.in>
2026-02-16 09:01:23 +0530

    UC3: Fix alignment using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,51 @@
+/**
+ * UC3 - OOPS Banner using String.join()
+ */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * *****    ***** ");
-        System.out.println("*     * *     * *             *");
-        System.out.println("*     * *     * *      *     * ");
-        System.out.println(" *****   *****   *       ***** ");
+        System.out.println(String.join("  ",
+                " *****  ",
+                " *****  ",
+                " ****** ",
+                " ****** "));
+
+        System.out.println(String.join("  ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     * "));
+
+        System.out.println(String.join("  ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*       "));
+
+        System.out.println(String.join("  ",
+                "*     * ",
+                "*     * ",
+                "******  ",
+                " *****  "));
+
+        System.out.println(String.join("  ",
+                "*     * ",
+                "*     * ",
+                "*       ",
+                "      * "));
+
+        System.out.println(String.join("  ",
+                "*     * ",
+                "*     * ",
+                "*       ",
+                "*     * "));
+
+        System.out.println(String.join("  ",
+                " *****  ",
+                " *****  ",
+                "*       ",
+                " *****  "));
     }
 }
